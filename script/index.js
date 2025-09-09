7960d1648dc22f0cfb5f427590ab2b40affdfe59
meiad-khan <meiadkhan615@gmail.com>
2025-09-09 09:32:19 +0600

    added spinner

--- script/index.js
+++ script/index.js
@@ -5,11 +5,93 @@ const loadLessons = ()=>{
 }
 
 
+const removeActive = ()=>{
+  const buttons = document.querySelectorAll(".lesson-btn");
+  // console.log(buttons);
+  buttons.forEach(btn=>{
+    btn.classList.remove("active");
+  })
+}
+
+
+const manageSpinner = (status)=>{
+  if(status){
+    document.getElementById('spinner').classList.remove('hidden');
+    document.getElementById('word-container').classList.add('hidden');
+  }else{
+    document.getElementById('word-container').classList.remove('hidden');
+    document.getElementById('spinner').classList.add('hidden');
+  }
+}
+
+
 const loadLevelWords = (id)=>{
+  manageSpinner(true);
   const url = `https://openapi.programming-hero.com/api/level/${id}`
   fetch(url)
   .then(res => res.json())
-  .then(data => displayWords(data.data));
+  .then(data => {
+    removeActive();
+    const btnClicked = document.getElementById(`btn-lesson-${id}`);
+    btnClicked.classList.add("active");
+    displayWords(data.data)
+  });
+}
+
+
+
+const loadWordDetail = async(id) =>{
+  const url =  `https://openapi.programming-hero.com/api/word/${id}`;
+  // console.log(url);
+  const res = await fetch(url);
+  const details = await res.json();
+  // console.log(details.data);
+  displayWordDetails(details.data);
+}
+
+
+// {
+// "word": "Eager",
+// "meaning": "আগ্রহী",
+// "pronunciation": "ইগার",
+// "level": 1,
+// "sentence": "The kids were eager to open their gifts.",
+// "points": 1,
+// "partsOfSpeech": "adjective",
+// "synonyms": [
+// "enthusiastic",
+// "excited",
+// "keen"
+// ],
+// "id": 5
+// }
+
+
+const createElement = (arr)=>{
+  const htmlElements = arr.map(el => `<span class ="btn">${el}</span>`);
+  return (htmlElements.join(" "));
+}
+
+const displayWordDetails = (word) =>{
+  // console.log(word);
+  const modalContainer = document.getElementById('modal-container');
+  // console.log(modalContainer);
+  modalContainer.innerHTML=`
+    <h2 class="text-2xl font-bold">${word.word} (<i class="fa-solid fa-microphone-lines"></i> :${word.pronunciation})</h2>
+    <h3 class="text-xl font-semibold">Meaning</h3>
+    <p>${word.meaning}</p>
+    <p class="text-xl font-semibold">Example</p>
+    <p>${word.sentence} </p>
+    <p>সমার্থক শব্দ গুলো</p>
+    <div class ="flex items-center gap-2">
+    ${createElement(word.synonyms)}
+    </div>
+  `;
+  // <span class="btn">${word.synonyms[0]} </span>
+  //   <span class="btn">${word.synonyms[1]}</span>
+  //   <span class="btn">${word.synonyms[2]}</span>
+
+  document.getElementById('my_modal_5').showModal();
 }
 
 const displayWords = (words)=>{
@@ -23,6 +105,7 @@ const displayWords = (words)=>{
       <h2 class="text-5xl font-bold ">নেক্সট Lesson এ যান</h2>
     </div>
     `;
+    manageSpinner(false);
     return;
   }
 // console.log(words);
@@ -35,16 +118,15 @@ words.forEach(word =>{
     <p class="font-semibold text-xl">Meaning /Pronounciation</p>
     <h3 class="font-bangla text-2xl font-medium">"${word.meaning? word.meaning:"শব্দ পাওয়া যায়নি"} / ${word.pronunciation? word.pronunciation: "pronunciation পাওয়া যায়নি"}"</h3>
     <div class="flex justify-between items-center">
-      <div><button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF75]"><i class="fa-solid fa-circle-info"></i></button></div>
+      <div><button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF75]"><i class="fa-solid fa-circle-info"></i></button></div>
       <div><button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF75]"><i class="fa-solid fa-volume-high"></i></button></div>
     </div>
 
     </div>
   `;
   wordContainer.appendChild(wordCard);
-
-
 })
+manageSpinner(false);
 }
 
 
@@ -56,7 +138,7 @@ for(let lesson of lessons){
   // console.log(lesson);
   const btnDiv = document.createElement('div');
   btnDiv.innerHTML =`
-  <button onclick = "loadLevelWords(${lesson.level_no})" class="btn btn-outline btn-primary">
+  <button id="btn-lesson-${lesson.level_no}" onclick = "loadLevelWords(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn">
   <i class="fa-solid fa-book-open"></i>Lesson-${lesson.level_no}
   </button>
   `;
